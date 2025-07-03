23b3107c4b132cf10a352e9ffe541611a79ed04f
Makabeyuuat <m.yuta.0328@icloud.com>
2025-07-03 10:52:12 +0900

    関数修正

--- src/differential_equations_dynamics.cpp
+++ src/differential_equations_dynamics.cpp
@@ -8,16 +8,16 @@ double f0 (const std::vector<double>& x) {
 }
 
 double f1 (const std::vector<double>& x) { 
-    return u1 * std::cos(x[3]); 
+    return u1 * std::cos(x[3] + x[4]); 
 }
 
 double f2 (const std::vector<double>& x) { 
-    return u1 * std::sin(x[3]); 
+    return u1 * std::sin(x[3] + x[4]); 
 }
 
 //theta
 double f3 (const std::vector<double>&x) { 
-    return u1 * std::tan(x[4])/lv; 
+    return u1 * std::-sin(x[5] - x[4])/(lv*std::cos(x[4])); 
 }
 
 //phi1
@@ -27,7 +27,7 @@ double f4 (const std::vector<double>&x) {
 
 //phi2
 double f5 (const std::vector<double>&x) {  
-    return u2; 
+    return u3; 
 }
 
 
@@ -36,7 +36,6 @@ double f6 (const std::vector<double>& x) {
 }
 
 double f7(const std::vector<double>& x) {
-    // x = [t,x,y,theta,phi, x_d,y_d,theta_d,phi_d]
     double u1_dot =  0.0;
     double theta   = x[3];
     double theta_d = x_d[3];
